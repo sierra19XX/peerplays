01d8372b9f4e20592115afcd5865ffaaf8ba0058
kstdl <kostello.d@gmail.com>
2017-12-12 18:15:55 +0300

    fix some tests. add gitlab-ci.yml

--- tests/common/database_fixture.cpp
+++ tests/common/database_fixture.cpp
@@ -89,11 +89,9 @@ database_fixture::database_fixture()
 
    boost::program_options::variables_map options;
 
-   genesis_state.initial_timestamp = time_point_sec( GRAPHENE_TESTING_GENESIS_TIMESTAMP );
-   //int back_to_the_past = 0;
-   //back_to_the_past = 7 * 24 * 60 * 60; // week
-   //genesis_state.initial_timestamp = time_point_sec( (fc::time_point::now().sec_since_epoch() - back_to_the_past) / GRAPHENE_DEFAULT_BLOCK_INTERVAL * GRAPHENE_DEFAULT_BLOCK_INTERVAL );
-   genesis_state.initial_parameters.witness_schedule_algorithm = GRAPHENE_WITNESS_SHUFFLED_ALGORITHM;
+   // genesis_state.initial_timestamp = time_point_sec( GRAPHENE_TESTING_GENESIS_TIMESTAMP );
+   genesis_state.initial_timestamp = time_point_sec( (fc::time_point::now().sec_since_epoch() / GRAPHENE_DEFAULT_BLOCK_INTERVAL) * GRAPHENE_DEFAULT_BLOCK_INTERVAL );
+//   genesis_state.initial_parameters.witness_schedule_algorithm = GRAPHENE_WITNESS_SHUFFLED_ALGORITHM;
 
    genesis_state.initial_active_witnesses = 10;
    for( unsigned i = 0; i < genesis_state.initial_active_witnesses; ++i )
