20116e0a941b6a517ca5aad93ed48c8a99d4d02f
cifer <maintianyu@gmail.com>
2018-02-25 12:14:01 +0800

    Fix #436 object_database created outside of witness data directory

--- libraries/chain/db_management.cpp
+++ libraries/chain/db_management.cpp
@@ -175,7 +175,9 @@ void database::reindex( fc::path data_dir )
 void database::wipe(const fc::path& data_dir, bool include_blocks)
 {
    ilog("Wiping database", ("include_blocks", include_blocks));
-   close();
+   if (_opened) {
+     close();
+   }
    object_database::wipe(data_dir);
    if( include_blocks )
       fc::remove_all( data_dir / "database" );
@@ -221,6 +223,7 @@ void database::open(
                     ("last_block->id", last_block)("head_block_id",head_block_num()) );
          reindex( data_dir );
       }
+      _opened = true;
    }
    FC_CAPTURE_LOG_AND_RETHROW( (data_dir) )
 }
@@ -263,6 +266,8 @@ void database::close(bool rewind)
       _block_id_to_block.close();
 
    _fork_db.reset();
+
+   _opened = false;
 }
 
 void database::force_slow_replays()
