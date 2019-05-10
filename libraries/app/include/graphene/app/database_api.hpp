720e35a10ad7e1b1d27bc8afa00a6dee99a69819
Peter Conrad <conrad@quisquis.de>
2019-05-10 13:15:15 +0200

    Externalized some API templates

--- libraries/app/include/graphene/app/database_api.hpp
+++ libraries/app/include/graphene/app/database_api.hpp
@@ -673,6 +673,8 @@ class database_api
 
 } }
 
+extern template class fc::api<graphene::app::database_api>;
+
 FC_REFLECT( graphene::app::order, (price)(quote)(base) );
 FC_REFLECT( graphene::app::order_book, (base)(quote)(bids)(asks) );
 FC_REFLECT( graphene::app::market_ticker, (base)(quote)(latest)(lowest_ask)(highest_bid)(percent_change)(base_volume)(quote_volume) );
