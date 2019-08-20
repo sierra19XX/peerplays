180ff226dcc452d78980120074133e32dd709cd0
Ronak Patel <r.patel@pbsa.info>
2019-08-20 13:07:29 +0530

    Fixed compilation error for max transaction size test case and genesis timestamp divisible by 5

--- tests/tests/network_broadcast_api_tests.cpp
+++ tests/tests/network_broadcast_api_tests.cpp
@@ -9,7 +9,10 @@
 #include <graphene/chain/proposal_object.hpp>
 #include <graphene/chain/witness_object.hpp>
 #include <graphene/chain/protocol/committee_member.hpp>
+#include <graphene/chain/protocol/sport.hpp>
 #include <fc/crypto/digest.hpp>
+#include <graphene/app/database_api.hpp>
+#include <graphene/app/api.hpp>
 
 #include "../common/database_fixture.hpp"
 
