OneGramTech/onegram-core

Separate exception declaration and implementation

libraries/protocol/small_ops.cpp
@@ -24,10 +24,35 @@
 
 #include <graphene/protocol/balance.hpp>
 #include <graphene/protocol/buyback.hpp>
+#include <graphene/protocol/exceptions.hpp>
 #include <graphene/protocol/fba.hpp>
 #include <graphene/protocol/vesting.hpp>
 
 #include <fc/io/raw.hpp>
+
+namespace graphene { namespace protocol {
+
+FC_IMPLEMENT_EXCEPTION( protocol_exception, 4000000, "protocol exception" )
+
+FC_IMPLEMENT_DERIVED_EXCEPTION( transaction_exception,      protocol_exception, 4010000,
+                                "transaction validation exception" )
+
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_missing_active_auth,     transaction_exception, 4010001,
+                                "missing required active authority" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_missing_owner_auth,      transaction_exception, 4010002,
+                                "missing required owner authority" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_missing_other_auth,      transaction_exception, 4010003,
+                                "missing required other authority" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_irrelevant_sig,          transaction_exception, 4010004,
+                                "irrelevant signature included" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_duplicate_sig,           transaction_exception, 4010005,
+                                "duplicate signature included" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( invalid_committee_approval, transaction_exception, 4010006,
+                                "committee account cannot directly approve transaction" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( insufficient_fee,           transaction_exception, 4010007, "insufficient fee" )
+
+} } // graphene::protocol
+
 
 GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::balance_claim_operation )
 GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::buyback_account_options )

sierra19XX/peerplays

libraries/chain/protocol/small_ops.cpp
@@ -31,6 +31,29 @@
 
 #include <fc/io/raw.hpp>
 
+namespace graphene { namespace chain {
+
+FC_IMPLEMENT_EXCEPTION( protocol_exception, 4000000, "protocol exception" )
+
+FC_IMPLEMENT_DERIVED_EXCEPTION( transaction_exception,      protocol_exception, 4010000,
+                                "transaction validation exception" )
+
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_missing_active_auth,     transaction_exception, 4010001,
+                                "missing required active authority" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_missing_owner_auth,      transaction_exception, 4010002,
+                                "missing required owner authority" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_missing_other_auth,      transaction_exception, 4010003,
+                                "missing required other authority" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_irrelevant_sig,          transaction_exception, 4010004,
+                                "irrelevant signature included" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( tx_duplicate_sig,           transaction_exception, 4010005,
+                                "duplicate signature included" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( invalid_committee_approval, transaction_exception, 4010006,
+                                "committee account cannot directly approve transaction" )
+FC_IMPLEMENT_DERIVED_EXCEPTION( insufficient_fee,           transaction_exception, 4010007, "insufficient fee" )
+
+} } // graphene::chain
+
 GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::balance_claim_operation )
 GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::buyback_account_options )
 GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::fba_distribute_operation )